747c208f7ea1a4d22199098a0b842c7b2de61cf8
P-Teo <162124101+P-Teo@users.noreply.github.com>
2024-04-06 18:34:43 +0300

    Add files via upload

---
+++ L3/3.1/main1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int x;
+	printf( "Scrie x: " );
+	scanf("%d",&x);
+	printf("\n %d\n",x);
+	
+
+	return x;
+}
---
+++ L3/3.1/main2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+
+int main( void )
+{
+
+	puts("Mesaj");
+	
+	return 1000;
+}
